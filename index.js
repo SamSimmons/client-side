9a7ea7b4842ef80c62e053f92f7559bef2b0e2c5
sam <samuel.j.simmons@gmail.com>
2016-04-04 12:23:18 +1200

    Updates

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var xhr = require('xhr')
-var example = require('./views/example.hbs')
+var nasa = require('./views/nasa.hbs')
+var cat = require('./views/cat.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
+xhr.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', function(err, data) {
   if (err) console.log(err) // do something
 
-  console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
+  var result = JSON.parse(data.body)
+  
+  document.querySelector('.wrapper').innerHTML = nasa(result)
+})
+
+var catButton = document.querySelector('.cat-btn')
+catButton.addEventListener('click', function() {
+  xhr.get('http://localhost:3000/v1/cats/', function (err, data) {
+
+    var results = JSON.parse(data.body)
+    document.querySelector('.wrapper').innerHTML = cat(results)
+    catButton.remove()
+
+  })
 })
 
